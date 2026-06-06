fe542252bd29c4dce2ca04f4ba9731d66fce3f2f
JoshuaDotExe <joshuawilliamswebster@gmail.com>
2026-06-07 00:57:15 +0100

    Removed test user

--- db/init/002_create_user_tables.sql
+++ db/init/002_create_user_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
@@ -15,20 +15,3 @@ CREATE TABLE IF NOT EXISTS user_credentials (
     password_changed_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO users (id, first_name, last_name, email, username)
-VALUES (
-    '11111111-2222-3333-4444-555555555555',
-    'Test',
-    'User',
-    'testuser@example.com',
-    'testuser'
-)
-ON CONFLICT (username) DO NOTHING;
-
-INSERT INTO user_credentials (user_id, password_hash, password_algo)
-VALUES (
-    '11111111-2222-3333-4444-555555555555',
-    '$argon2id$v=19$m=65536,t=3,p=2$and0c2VlZHN0YXRpY3NhbHQ$tJ1IZjpbKmSb1z+LnhKeo7I6bQXXht+r5czuAZCg3y0',
-    'argon2id'
-)
-ON CONFLICT (user_id) DO NOTHING;
